2a0af7d7717cd89667388f382a29003499681a65
alex <pluscda@gmail.com>
2021-06-16 14:17:39 +0800

    g

--- src/layouts/indexWithSidebar.vue
+++ src/layouts/indexWithSidebar.vue
@@ -1,28 +1,52 @@
 <template>
   <section class="overflow-x-hidden index-position flex flex-col space-y-12 items-center pt-7">
-    <div v-for="(item, i) in icons" @click="showMenu($event, item)" :key="i">
-      <img :src="item" class="block" width="30" height="27" />
+    <div v-for="(item, i) in icons" @click="showMenu($event, item, i)" :key="i">
+      <img :src="item" class="block cursor-pointer" width="30" height="27" />
     </div>
+    <nav class="my-menuw">
+      <div v-for="(item, i) in topMenuu" :key="i">
+        <h4>
+          <h5 style="cursor: pointer">{{ item }}</h5>
+          <section v-if="activeTab == i">
+            <div v-for="(row, j) in rows[i].split(',')" :key="'row' + j" style="background: #d3dceb" class="sub-menu !py-2">
+              <p style="cursor: pointer" class="ml-6" v-html="row"></p>
+            </div>
+          </section>
+        </h4>
+      </div>
+    </nav>
   </section>
 </template>
 
 <script>
 const icons = ["management.png", "application.png", "mailbox.png", "reviewarea.png", "search.png", "auditstatistics.png"];
+const topMenuu = ["系統管理", "電子表單申請", "個人資料區", "審核資料區", "電子表單查詢", "稽核統計作業"];
+const row0 = `帳號管理, 組織單位管理, 職稱管理,角色管理,系統功能管理,電子表單設定,簽核片語設定`;
+const row1 = `生物安全等級實驗室證明, 教育訓練證明書, 送審證明書,基因重組實驗申請書,生物材料申請書</span>`;
+const row2 = `申請資料匣`;
+const row3 = `待審核資料匣,已審核資料匣`;
+const row4 = `電子表單通用查詢作業`;
+const row5 = `交易歷程,一般統計查詢`;
+const rows = [row0, row1, row2, row3, row4, row5];
 export default {
   name: "indexwidthbar2",
   data() {
     return {
       dtcCurrentCmp: "",
       icons,
+      topMenuu,
+      rows,
+      activeTab: -1,
     };
   },
   components: {},
   methods: {
-    showMenu(evt, item) {
+    showMenu(evt, item, idx) {
       [...document.querySelectorAll(".dtc-heightlight-tab")].forEach((s) => {
         s.classList.remove("dtc-heightlight-tab");
       });
       evt.target.classList.add("dtc-heightlight-tab");
+      this.activeTab = idx;
     },
   },
   mounted() {},
@@ -47,5 +71,26 @@ export default {
 
 .left-nav {
   max-height: calc(100vh - 80px);
+}
+
+.my-menuw {
+  position: fixed !important;
+  height: calc(100vh - 80px) !important;
+  top: 32px;
+  left: 63px;
+  width: 251px;
+  background: #eff7ff;
+  display: block;
+  flex-direction: column;
+  z-index: 9999999;
+  text-align: left;
+  font: normal normal medium 24px/35px Source Han Sans TW;
+  letter-spacing: 2.4px;
+  color: #0d2868;
+  padding: 15px;
+  overflow-y: auto;
+}
+.sub-menu {
+  box-shadow: 20px 0px 0 #d3dceb, -20px 0px 0 #d3dceb;
 }
 </style>
