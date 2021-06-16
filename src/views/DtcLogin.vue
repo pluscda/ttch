45fdd5e3fdcf946dbc8b63c87e05bd95ab277393
alex <pluscda@gmail.com>
2021-06-16 09:53:19 +0800

    g

--- src/views/DtcLogin.vue
+++ src/views/DtcLogin.vue
@@ -1,21 +1,27 @@
 <template>
   <section class="relative grid place-items-center !bg-white" style="width: 100vw; height: 100vh">
-    <div class="absolute inset-1 z-0">
+    <div class="absolute inset-3 z-0">
       <img src="logo.png" />
     </div>
     <div class="flex flex-col dtc-box relative login-box z-10" data-msg="生物安全會電子表單系統">
-      <img src="login.png" class="mt-6 object-contain block self-center" width="190" height="40" />
-      <div class="dtc-input ml-20 flex space-x-2 mt-6">
+      <img src="login.png" class="mt-10 object-contain block self-center" width="190" height="40" />
+      <div class="dtc-input ml-20 flex space-x-2 mt-10">
         <img src="user.png" class="object-contain block" width="40" height="40" />
-        <InputText type="text" v-model="value1" class="z-20 !bg-white w-60" placeholder="請輸入帳號"></InputText>
+        <InputText type="text" v-model="value1" class="z-20 !bg-white w-64" placeholder="請輸入帳號"></InputText>
       </div>
-      <div class="dtc-input ml-20 flex space-x-2 mt-6">
+      <div class="dtc-input ml-20 flex space-x-2 mt-10">
         <img src="password.png" class="object-contain block" width="40" height="40" />
-        <InputText placeholder="請輸入密碼" type="password" v-model="pwd" class="z-20 !bg-white w-60" />
+        <InputText placeholder="請輸入密碼" type="password" v-model="pwd" class="z-20 !bg-white w-64" />
       </div>
-      <div class="dtc-input ml-20 flex space-x-2 mt-6">
+      <div class="dtc-input ml-20 flex space-x-2 mt-10">
         <img src="verificationcode.png" class="object-contain block" width="40" height="40" />
         <InputText placeholder="請輸入驗證碼" type="text" v-model="pass2" class="z-20 !bg-white w-28" />
+        <img src="turn.png" class="object-contain block" width="30" height="30" />
+      </div>
+
+      <div class="dtc-input ml-36 flex space-x-6 mt-10">
+        <Button>確認登入</Button>
+        <Button class="p-button-secondary">重新輸入</Button>
       </div>
     </div>
   </section>
