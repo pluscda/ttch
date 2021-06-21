75d0b25aad2095f60a87313f4fea2dbc8a3b7fc7
alex <pluscda@gmail.com>
2021-06-21 11:42:34 +0800

    g

--- src/components/AccountAddDlg.vue
+++ src/components/AccountAddDlg.vue
@@ -6,8 +6,34 @@
 
     <template v-slot:body>
       <main class="grid grid-cols-[400px,400px] gap-[10px]">
-        <div class="w-full"></div>
-        <div class="w-full"></div>
+        <div class="w-full text-left space-y-2">
+          <header class="bg-[#D5E3EA] h-[35px] color-[#0A2548] leading-[35px] pl-2">帳號資料</header>
+          <DtxInputGroup prepend="帳號編號">
+            <el-input placeholder="請輸入帳號編號"></el-input>
+          </DtxInputGroup>
+          <DtxInputGroup prepend="帳號編號">
+            <el-input placeholder="請輸入帳號編號"></el-input>
+          </DtxInputGroup>
+          <DtxInputGroup prepend="帳號編號">
+            <el-input placeholder="請輸入帳號編號"></el-input>
+          </DtxInputGroup>
+          <DtxInputGroup prepend="帳號編號">
+            <el-input placeholder="請輸入帳號編號"></el-input>
+          </DtxInputGroup>
+          <DtxInputGroup prepend="帳號編號">
+            <el-input placeholder="請輸入帳號編號"></el-input>
+          </DtxInputGroup>
+          <DtxInputGroup prepend="帳號編號">
+            <el-input placeholder="請輸入帳號編號"></el-input>
+          </DtxInputGroup>
+        </div>
+        <div class="w-full text-left space-y-2">
+          <header class="bg-[#D5E3EA] h-[35px] color-[#0A2548] leading-[35px] pl-2">授權角色</header>
+          <div v-for="(item, i) in roles" :key="i" class="grid grid-cols-[100px,1fr] pl-2 border-b-[1px] border-b-gray-100">
+            <Checkbox :id="'role' + i" :value="item" />
+            <label :for="'role' + i">{{ item }}</label>
+          </div>
+        </div>
       </main>
     </template>
 
@@ -20,10 +46,12 @@
 <script>
 import { closeDlg$ } from "/@/store";
 import Dlg from "./Dlg.vue";
+const roles = ["系統管理者", "系統管理者", "系統管理者", "系統管理者", "系統管理者", "系統管理者", "系統管理者", "系統管理者"];
 export default {
   data() {
     return {
       closeDlg$,
+      roles,
     };
   },
   components: {
